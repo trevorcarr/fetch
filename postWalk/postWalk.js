d1e3fb305e0b8eac7047dc957cd4a0b9cb036387
Trevor Carr <sicktightyo@gmail.com>
2017-03-28 18:01:26 +0100

    Added walk stats

--- postWalk/postWalk.js
+++ postWalk/postWalk.js
@@ -8,8 +8,27 @@
       function initMap() {
         var map = new Map('post_map');
         var path = (new Helper().getUrlParameter('path'));
-        console.log(decodeURI(path));
+//        console.log(decodeURI(path));
         map.setPathJson(decodeURIComponent(path));
+        tripStats(map);
       }
       
+      function tripStats(map) {
+        distance = map.getLengthOfPathInM();
+        distance = fixDistance(distance);
+        document.getElementById("distance").innerHTML = "Distance: " + distance + " km";
+        document.getElementById("minutes").innerHTML = "Minutes: " ;
+        console.log(distance);
+        document.getElementById("price").innerHTML = "Price: " + "£" + (distance*10);
+        
+        
+      }
+      
+      function fixDistance(distance){
+        distance = Math.round(distance);
+        length = distance.toString().length;
+        distance = (distance / 1000).toFixed(2);
+        return distance;
+        
+      }
       window.addEventListener("load", initMap);
